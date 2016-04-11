220cc4261170356e1eb5851cd699d3ce1aac031d
Terence Lim <terencelimz@gmail.com>
2016-04-11 17:07:25 +0800

    added new functionality

--- nextBus.py
+++ nextBus.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 
 class NextBus(object):
 
@@ -10,6 +11,7 @@ class NextBus(object):
 
 	def save_as_json(self):
 		response = self.getAllBusStops()
+		response['last_updated'] = time.strftime("%d %b %Y %H:%M:%S" , time.localtime())
 		with open('busStops.json', 'w') as outfile:
 			json.dump(response, outfile)
 			print('Success')
@@ -18,3 +20,20 @@ class NextBus(object):
 		url ="https://nextbus.comfortdelgro.com.sg/eventservice.svc/Shuttleservice?busstopname="+name
 		r=requests.get(url)
 		return r.json()
+
+	def getAllBusTimings(self):
+		with open('busStops.json') as data_file:
+			data = json.load(data_file)
+
+		data_result = {'time':time.strftime("%d %b %Y %H:%M:%S" , time.localtime())}
+		
+		names = [r['name'] for r in data['BusStopsResult']['busstops']]
+
+		results = []
+
+		for name in names:
+			result = self.getBusTiming(name)['ShuttleServiceResult']
+			results.append(result)
+		data_result['results'] = results
+
+		return data_result
---
+++ save_bus_stops.py
@@ -0,0 +1,4 @@
+import nextBus
+
+if __name__ == '__main__':
+	nextBus.NextBus().save_as_json()
